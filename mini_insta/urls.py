f34400eb38d033c6ebd70b2832f0dbc62aabdadb
Mia Batista <miajolie@bu.edu>
2025-09-25 17:01:48 -0400

    finished the website

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path 
-from .views import ProfileListView
+from .views import ProfileListView, ProfileDetailView
 
 urlpatterns=[
     
     path('', ProfileListView.as_view(), name = "show_all_profiles"),
-
-    # path('', RandomArticleView.as_view(), name = "random"),
-    # path('article/<int:pk>', ArticleView.as_view(), name='article'),
+    
+    path('profile/<int:pk>', ProfileDetailView.as_view(), name='show_profile'),
 
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -10,3 +10,10 @@ class ProfileListView(ListView):
     model = Profile
     template_name = "mini_insta/show_all_profiles.html"
     context_object_name = "profiles"
+
+class ProfileDetailView(DetailView):
+    '''display a single profile'''
+
+    model = Profile
+    template_name = "mini_insta/show_profile.html"
+    context_object_name = "profile" #singular variable name
